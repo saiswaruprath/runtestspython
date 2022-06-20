2e624dbe878950806b74b6e6a1ee1abb9adf2dd5
Sai Swarup Rath <saiswaruprath7@gmail.com>
2022-06-20 10:55:24 -0700

    Create test_account.py

---
+++ testfixtures/test_account.py
@@ -0,0 +1,53 @@
+"""
+Test Cases TestAccountModel
+"""
+import json
+from unittest import TestCase
+from models import db
+from models.account import Account
+
+ACCOUNT_DATA = {}
+
+class TestAccountModel(TestCase):
+    """Test Account Model"""
+
+    @classmethod
+    def setUpClass(cls):
+        """ Connect and load data needed by tests """
+        db.create_all()
+        global ACCOUNT_DATA
+        with open('tests/fixtures/account_data.json') as json_data:
+            ACCOUNT_DATA = json.load(json_data)
+
+    @classmethod
+    def tearDownClass(cls):
+        """Disconnect from database"""
+        db.session.close()
+
+    def setUp(self):
+        """Truncate the tables"""
+        db.session.query(Account).delete()
+        db.session.commit()
+        
+
+    def tearDown(self):
+        """Remove the session"""
+        db.session.remove()
+
+    ######################################################################
+    #  T E S T   C A S E S
+    ######################################################################
+
+    def test_create_an_Account(self):
+        """Test create a single Account """
+        data = ACCOUNT_DATA[0]
+        account = Account(**data)
+        account.create()
+        self.assertEqual(len(Account.all()),1)
+
+    def test_create_all_accounts(self):
+        """ Test creating multiple Accounts """
+        for data in ACCOUNT_DATA:
+            account = Account(**data)
+            account.create()
+        self.assertEqual(len(Account.all()), len(ACCOUNT_DATA))    
