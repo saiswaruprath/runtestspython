6402b1ddb96fc053e50f8c3a57ed348c920948b9
Sai Swarup Rath <saiswaruprath7@gmail.com>
2022-06-19 23:50:11 -0700

    Update testassertion.py

--- testassertion.py
+++ testassertion.py
@@ -40,3 +40,31 @@ class TestStack(TestCase):
         self.assertTrue(self.stack.is_empty())
         self.stack.push(5)
         self.assertFalse(self.stack.is_empty())
+
+        
+"""Implements a Stack data structure"""
+from typing import Any
+
+
+class Stack:
+    """Implements a Stack data structure"""
+
+    def __init__(self):
+        """Constructor"""
+        self.items = [] 
+    
+    def push(self, data: Any) -> None: 
+        """Places an item onto the stack"""
+        self.items.append(data) 
+    
+    def pop(self) -> Any:
+        """Removes an item from the stack and returns it"""
+        return self.items.pop() 
+        
+    def peek(self) -> Any:
+        """Returns the item at the top of the stack without removing it"""
+        return self.items[-1] 
+        
+    def is_empty(self) -> bool:
+        """Returns True if the stack is empty, otherwise returns False"""
+        return len(self.items) == 0        
